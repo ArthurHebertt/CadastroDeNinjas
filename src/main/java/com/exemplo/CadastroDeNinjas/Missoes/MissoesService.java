e55c111a9d633ca6bd716aa6290e1e2271c918ce
ArthurHebertt <you@example.com>
2026-06-12 22:05:57 -0300

    fix: tira a responsabilidade do meu missoesModel e passa para o meu missoesDTO usando o streamns no map

--- src/main/java/com/exemplo/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/com/exemplo/CadastroDeNinjas/Missoes/MissoesService.java
@@ -3,24 +3,46 @@ package com.exemplo.CadastroDeNinjas.Missoes;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MissoesService {
 
     private MissoesRepository missoesRepository;
 
-    public MissoesService(MissoesRepository missoesRepository) {
+    private MissoesMapper missoesMapper;
+
+    public MissoesService(MissoesRepository missoesRepository, MissoesMapper missoesMapper) {
         this.missoesRepository = missoesRepository;
+        this.missoesMapper = missoesMapper;
     }
 
     // Criar Missoes
-    public MissoesModel criarMissoes (MissoesModel missao) {
-        return missoesRepository.save(missao);
+    public MissoesDTO criarMissoes (MissoesDTO missaoDTO) {
+        MissoesModel missao = missoesMapper.map(missaoDTO);
+        missao = missoesRepository.save(missao);
+        return missoesMapper.map(missao);
     }
 
     // Listar todas as missoes
-    public List<MissoesModel> listarMissoes() {
-        return missoesRepository.findAll();
+    public List<MissoesDTO> listarMissoes() {
+        List<MissoesModel> missoes = missoesRepository.findAll();
+        return missoes.stream()
+                .map(missoesMapper::map)
+                .collect(Collectors.toList());
+    }
+
+    // Alterar missoes
+    public MissoesDTO alterarMissao (Long id, MissoesDTO missoesDTO){
+        Optional<MissoesModel> missaoExistente = missoesRepository.findById(id);
+        if (missaoExistente.isPresent()) {
+            MissoesModel missaoAtualizada = missoesMapper.map(missoesDTO);
+            missaoAtualizada.setId(id);
+            MissoesModel missaoSalva = missoesRepository.save(missaoAtualizada);
+            return missoesMapper.map(missaoSalva);
+
+        } return null;
     }
 
     // Deletar missoes
